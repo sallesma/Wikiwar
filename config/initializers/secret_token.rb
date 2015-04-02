6183186353943ae0045ce8917e2d7755d7f4ef96
Martin Salles <martin.salles@free.fr>
2015-04-02 22:36:14 +0200

    Initial commit, default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikiwar::Application.config.secret_token = 'bee892456a24f38a071e8818ff7ee48e41c89777167575e6fdbefa545345e915ecfb31f26920b598208ab4549039d2728111a5738bc32e69ae58701ebd4e829d'

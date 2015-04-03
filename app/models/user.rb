6172f45651b135bc9060a4abeba13d9d3afed76d
Martin Salles <martin.salles@free.fr>
2015-04-03 14:58:40 +0200

    Add email valdiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_uniqueness_of :pseudo
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
   
   def self.authenticate(pseudo, password)
     user = find_by_pseudo(pseudo)

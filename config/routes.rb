fc7642e002e04b29983a2d19d7be8bd0401f0072
Martin Salles <martin.salles@free.fr>
2015-04-03 09:15:17 +0200

    Add index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Wikiwar::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "welcome#index"
   resources :users
   resources :sessions
 

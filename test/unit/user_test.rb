bb0ce185b6baebdcc98998e151a8d9c3a6097c26
Martin Salles <martin.salles@free.fr>
2015-04-03 15:46:44 +0200

    Add basic user unit test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save user without email" do
+    user = User.new
+    user.pseudo = "test"
+    assert !user.save, "Saved the user without an email"
+  end
+
+  test "should not save user without pseudo" do
+    user = User.new
+    user.email = "test@test.test"
+    assert !user.save, "Saved the user without a pseudo"
+  end
 end

46a1072165fc8738ae652d17749a3b2f480de6dd
Martin Salles <martin.salles@free.fr>
2015-04-03 16:32:12 +0200

    Visitors can't access pages that need login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
+
+    def authenticate_user
+      if current_user.nil?
+        flash[:error] = 'You must be signed in to view that page.'
+        redirect_to :root
+      end
+    end
 end

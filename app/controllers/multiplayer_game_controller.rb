708b68127f9b7e033e99df2e815d75f798658a48
Martin Salles <martin.salles@free.fr>
2015-04-12 12:32:20 +0200

    Add search player functionality to multiplayer

--- app/controllers/multiplayer_game_controller.rb
+++ app/controllers/multiplayer_game_controller.rb
@@ -1,7 +1,10 @@
 class MultiplayerGameController < GameController
-    before_filter :authenticate_user
+  before_filter :authenticate_user
 
-    def index
-    @suggested_users = User.all.first(10)
+  def index
+    if params.has_key?("find") and not params[:find].empty?
+      @found_users = User.where("pseudo LIKE ?", "%#{params[:find]}%").first(10)
     end
+    @suggested_users = User.all.first(10)
+  end
 end

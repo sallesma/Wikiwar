45e7a0e3d65d77ba7ad5d193d2bb067e9e329a1f
Martin Salles <martin.salles@free.fr>
2015-04-20 12:22:04 +0200

    Fix tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -6,17 +6,4 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
     assert_template :new
   end
-
-  test "should get edit" do
-    get :edit
-    assert_response :success
-    assert_template :edit
-  end
-
-  test "should get statistics" do
-    get :statistics
-    assert_response :success
-    assert_template :statistics
-  end
-
 end
--- test/unit/helpers/game_helper_test.rb
+++ test/unit/helpers/game_helper_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class GameHelperTest < ActionView::TestCase
+    include GameHelper
+
+    def test_is_finished
+        assert is_finished("foo", "foo")
+    end
 end

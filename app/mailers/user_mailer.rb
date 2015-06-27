fdb13f5c16ecc190bad71019bfbc0276cfec56b6
Martin Salles <martin.salles@free.fr>
2015-06-27 21:03:27 +0200

    Fix email urls

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ActionMailer::Base
 
   def reset_password_email(user)
     @user = user
-    @password_reset_url = 'http://localhost:3000/password_reset?' + @user.password_reset_token
+    @password_reset_url = password_reset_url + '?' + @user.password_reset_token
     mail(:to => user.email, :subject => 'Password Reset Instructions')
   end
 end
